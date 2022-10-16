056df02c5d8d7c80dfd80386e0777c2b1aaab8d7
Derrick Bol <derrickbol33@gmail.com>
2022-10-16 23:12:35 +0800

    fix: correcly load images

--- app/routes/photography.tsx
+++ app/routes/photography.tsx
@@ -26,40 +26,54 @@ export const loader: LoaderFunction = async () => {
   return images;
 };
 
+const BlurredImage = (props: any) => {
+  const [imageLoaded, setLoaded] = React.useState(false);
+  const imageRef = React.useRef<HTMLImageElement>(null);
+
+  React.useEffect(() => {
+    if (imageRef.current?.complete) {
+      setLoaded(true);
+    }
+  }, []);
+
+  return (
+    <figure
+      style={{
+        backgroundImage: `url(${props.image.lqip})`,
+        paddingTop: `calc(100% / ${props.image.aspectRatio})`,
+        backgroundRepeat: "no-repeat",
+      }}
+      className="relative m-0 w-full bg-black bg-center bg-cover bg-no-repeat"
+    >
+      {
+        <img
+          ref={imageRef}
+          alt={props.image.alt}
+          srcSet={props.image.srcSet}
+          sizes={props.image.sizes}
+          style={{
+            opacity: imageLoaded ? 1 : 0,
+            transition: "opacity 1s ease-in",
+          }}
+          src={props.image.src}
+          className="absolute top-0 left-0 w-full h-full object-cover object-center"
+          onLoad={() => {
+            setLoaded(true);
+          }}
+        />
+      }
+    </figure>
+  );
+};
+
 export default function Photography() {
   const urls = useLoaderData();
-  const [imageLoaded, setLoaded] = React.useState(false);
 
   return (
     <div>
-      <main>
+      <main className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {urls.map((image: ReturnType<typeof getImageProps>, index: number) => (
-          <figure
-            key={image.id + index}
-            style={{
-              backgroundImage: `url(${image.lqip})`,
-              paddingTop: `calc(100% / ${image.aspectRatio})`,
-              backgroundRepeat: "no-repeat",
-            }}
-            className="relative m-0 w-full bg-black bg-cover bg-no-repeat"
-          >
-            {
-              <img
-                alt={image.alt}
-                srcSet={image.srcSet}
-                sizes={image.sizes}
-                style={{
-                  opacity: imageLoaded ? 1 : 0,
-                  transition: "opacity 1s ease-in",
-                }}
-                src={image.src}
-                className="absolute top-0 left-0 w-full"
-                onLoad={() => {
-                  setLoaded(true);
-                }}
-              />
-            }
-          </figure>
+          <BlurredImage key={index} image={image} />
         ))}
       </main>
     </div>
