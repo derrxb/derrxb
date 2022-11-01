28e63464aa213c88543072d8b564d7db10662748
Derrick Bol <derrickbol33@gmail.com>
2022-11-01 19:31:57 +0800

    chore: remove unused items from sanity

--- images/schemas/schema.js
+++ images/schemas/schema.js
@@ -1,14 +1,8 @@
 // First, we must import the schema creator
 import createSchema from "part:@sanity/base/schema-creator";
-
 // Then import schema types from any plugins that might expose them
 import schemaTypes from "all:part:@sanity/base/schema-type";
-
-// We import object and document schemas
-import author from "./author";
 import blockContent from "./blockContent";
-import category from "./category";
-import post from "./post";
 import stories from "./stories";
 
 // Then we give our schema to the builder and provide the result to Sanity
@@ -20,9 +14,6 @@ export default createSchema({
   types: schemaTypes.concat([
     // The following are document types which will appear
     // in the studio.
-    post,
-    author,
-    category,
     stories,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
